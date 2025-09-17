16f43e563939c67e96c5ee4dbf5e20382bd3d200
jiajian.chi <jiajian.chi@zstack.io>
2025-09-17 15:27:41 +0800

    improvement: add client GetZone func

--- pkg/client/zone_actions.go
+++ pkg/client/zone_actions.go
@@ -12,3 +12,9 @@ func (cli *ZSClient) QueryZone(params param.QueryParam) ([]view.ZoneView, error)
 	resp := make([]view.ZoneView, 0)
 	return resp, cli.List("v1/zones", &params, &resp)
 }
+
+// GetZone by uuid
+func (cli *ZSClient) GetZone(uuid string) (*view.ZoneView, error) {
+	var resp view.ZoneView
+	return &resp, cli.Get("v1/zones/", uuid, nil, &resp)
+}
